cc93870ecc5e9112acdec0d3361850a1000a4619
Shal9981 <132664219+Shal9981@users.noreply.github.com>
2023-05-05 23:16:14 -0400

    Update streamlit_app.py
    My mom healthy dinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My MOM New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & rocket smoothie')
-streamlit.text('Hard-Boiled Free-renge egg')
+streamlit.header('Breakfast Favourite')
+streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-renge egg')
+streamlit.text('🥑🍞 Avacado toast')
